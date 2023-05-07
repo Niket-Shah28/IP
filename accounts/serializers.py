bddb5021b595c0774a5bb043b355c4b364b3f867
Dev Patel <devpatel0732@gmail.com>
2023-05-07 16:30:31 +0530

    debug02

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -184,7 +184,7 @@ class ScoreSerializer(serializers.ModelSerializer):
 
     class Meta:
         model= Score
-        fields= ['stack','question_no', 'rating']
+        fields= ['sapid','stack','question_no', 'rating']
     
     def create(self, validated_data):
 
